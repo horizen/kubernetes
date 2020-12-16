4e5bfb319408afcac97ce8472dcf9e0857e0d367
willyao <willyao@tencent.com>
2020-12-16 19:40:05 +0800

    container interface name configable

--- pkg/kubelet/dockershim/network/network.go
+++ pkg/kubelet/dockershim/network/network.go
@@ -16,8 +16,15 @@ limitations under the License.
 
 package network
 
-// TODO: Consider making this value configurable.
-const DefaultInterfaceName = "eth0"
+import (
+	"os"
+)
+
+// the container network infacename will be readed from the config that the systemd used to start the kubelet service
+// if there is no value from the config , the name will be set in default value "eth0"
+// there may can provide a interface to get the name from the TKE console
+// if need change the name in runtime , the pods created before changing need restart
+var DefaultInterfaceName = getInterfaceNameFromEnv()
 
 // CNITimeoutSec is set to be slightly less than 240sec/4mins, which is the default remote runtime request timeout.
 const CNITimeoutSec = 220
@@ -25,3 +32,11 @@ const CNITimeoutSec = 220
 // UseDefaultMTU is a marker value that indicates the plugin should determine its own MTU
 // It is the zero value, so a non-initialized value will mean "UseDefault"
 const UseDefaultMTU = 0
+
+func getInterfaceNameFromEnv() string{
+	interfaceName := os.Getenv("CONTAINER_NETWORK_INTERFACE_NAME")
+	if interfaceName == "" {
+		interfaceName = "eth0"
+	}
+	return interfaceName
+}
